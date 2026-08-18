ca9946411a579d5fb79ff1718131862e394eb8b8
Alex <dok.go@yandex.ru>
2026-08-18 10:15:36 +0500

    fix

--- src/components/ui/EditLineText.svelte
+++ src/components/ui/EditLineText.svelte
@@ -55,7 +55,12 @@
     <button
             type="button"
             onclick={() => isActive = true}
-            class="cursor-pointer border-1 text-blue-500 border-blue-500  py-1 px-2 rounded-md transition-colors
+            class="cursor-pointer flex items-center gap-1 border text-blue-500 border-blue-500  py-1 px-2 rounded-md transition-colors
                  hover:bg-blue-500 hover:text-white"
-    >Добавить</button>
+    >
+        <span>Редактировать</span>
+        <svg class="w-4 h-4 mt-[3px] group-hover:text-blue-500 transition-colors" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
+        </svg>
+    </button>
 {/if}
